e85b849943d5b91ca223b2612a2f203e7a85bf55
Zachary Porter <zporter9@gmail.com>
2018-12-01 17:54:36 -0500

    Day 1: First working attempt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
+// cargo run -- "$(cat data/day-1_1.txt)"
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let freq_changes: Vec<&str> = args[1].split("\n").collect();
+    let mut current_freq: i32 = 0;
+
+    for change in freq_changes.iter() {
+        let amount: i32 = match change.parse() {
+            Ok(num) => num,
+            Err(_) => 0,
+        };
+
+        current_freq += amount;
+    }
+
+    println!("Current frequency: {}", current_freq);
 }
